c40765352c4e3daeadfcab8fb5284470276caed9
Pranayjeet Thakare <pranayjeetthakare@hotmail.com>
2018-11-20 16:28:16 -0800

    added statics and url handelling

---
+++ crawler/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from crawler import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    # path('', views.articles, name='articles'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
